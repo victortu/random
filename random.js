9b2fbe051ebf336d6dbdb210d0e8a253a1e2ebba
victortu <victormbea@gmail.com>
2015-07-07 18:22:45 +0200

    random version 2

--- random.js
+++ random.js
@@ -1,8 +1,9 @@
 //Math.random() devuelve un número aleatorio entre 0 y 1
 var numero = Math.random();
 
+var str = 'Mayor que 0.5';
 if(numero <= 0.5){
-  console.log('\n' + numero + 'Menor que 0,5 \n');
-}else{
-  console.log('\n' + numero + 'Menor que 0,5 \n');
+  str = 'Menor que 0.5';
 }
+
+console.log('\n'numero + str '\n');
